5cb789c85154443b6b32480c339989f8cf5e9b48
Alfurquan Zahedi <alzahedi@microsoft.com>
2025-03-25 11:42:01 +0000

    Added for storage

--- pricing-poc/src/main/scala/example/strategy/ReservedPaaSPricing.scala
+++ pricing-poc/src/main/scala/example/strategy/ReservedPaaSPricing.scala
@@ -37,8 +37,8 @@ class ReservedPaaSPricing extends PricingStrategy {
       .withColumn("sqlServiceTier", sqlTierExpr)
       .withColumn("sqlHardwareType", sqlHardwareExpr)
 
-    targetSkuExpandedDF.printSchema()
-    targetSkuExpandedDF.show(false)
+    // targetSkuExpandedDF.printSchema()
+    // targetSkuExpandedDF.show(false)
 
     val filteredPricingDF = pricingDf.filter(
        col("skuName") === "vCore" &&
@@ -118,6 +118,60 @@ class ReservedPaaSPricing extends PricingStrategy {
       .select(
         col("SkuRecommendationForServers.ServerName"),
         col("SkuRecommendationResults.TargetSku"))
+
+    val sqlTierExpr = expr(
+      "CASE " +
+        "WHEN originalSqlServiceTier = 'General Purpose' OR originalSqlServiceTier = 'Next-Gen General Purpose' THEN 'General Purpose' " +
+        "WHEN originalSqlServiceTier = 'Business Critical' THEN 'Business Critical' " +
+        "WHEN originalSqlServiceTier = 'Hyperscale' THEN 'Hyperscale' " +
+        "ELSE '' END"
+    ).alias("sqlServiceTier")
+
+    val targetSkuExpandedDF = flattenedDf
+      .select(col("ServerName"), col("TargetSku.Category.SqlServiceTier").alias("originalSqlServiceTier"), 
+        col("TargetSku.Category.HardwareType").alias("originalSqlHardwareType"),
+        col("TargetSku.StorageMaxSizeInMb").alias("storageMaxSizeInMb"),
+        col("TargetSku.Category.SqlTargetPlatform").alias("targetPlatform")
+      )
+      .withColumn("sqlServiceTier", sqlTierExpr)
+      .withColumn("storageMaxSizeInGb", col("storageMaxSizeInMb") / 1024)
+    
+    targetSkuExpandedDF.show(false)
+
+    val filteredPricingDF = pricingDf.filter(
+       col("location") === "US West" &&
+       col("type") === PricingType.Consumption.toString 
+    )
+    val targetAlias = "target"
+    val pricingAlias = "pricing"
+
+    val joinedDF = targetSkuExpandedDF.as(targetAlias)
+      .join(
+        filteredPricingDF.as(pricingAlias),
+        col(s"$pricingAlias.skuName") === col(s"$targetAlias.sqlServiceTier"),
+        "inner"
+    )
+    
+    val resultDF = joinedDF
+      .orderBy(col(s"$pricingAlias.retailPrice").asc)
+      .limit(1)
+      .select(col("retailPrice"), col("storageMaxSizeInGb"), col("targetPlatform"))
+      .withColumn(
+        "storageCost",
+         bround(
+          when(col("targetPlatform") === PlatformType.AzureSqlManagedInstance.toString,
+            col("retailPrice") * greatest(col("storageMaxSizeInGb") - 32, lit(0)) // Handles max(storageMaxSizeInGb - 32, 0)
+          ).otherwise(
+            col("retailPrice") * (col("storageMaxSizeInGb") * 1.3)
+          ), 
+        2)
+      )
+      .drop("retailPrice")
+      .drop("storageMaxSizeInGb")
+      .drop("targetPlatform")
+      .toDF()
+    
+    resultDF.show(false)
 
     val sqlServiceTierOpt: Option[String] = flattenedDf
       .select(col("TargetSku.Category.SqlServiceTier"))
